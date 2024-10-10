fc301958bef66f10b56cea2227ebfdb23af69179
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-10-10 12:07:09 -0600

    models

--- server/models.py
+++ server/models.py
@@ -4,3 +4,39 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    name = db.Column(db.String)
+
+class Textbook(db.Model, SerializerMixin):
+    __tablename__ = "textbooks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String)
+    title = db.Column(db.String)
+    img = db.Column(db.String)
+
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(users.id), nullablle=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(posts.id), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+
+class Post(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    textbook_id = db.Column(db.Integer, db.ForeignKey(textbooks.id), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(users.id), nullable=False)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+class Watchlist(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey(posts.id))
+    textbook_id = db.Column(db.Integer, db.ForeignKey(textbooks.id))
