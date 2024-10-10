10aeae944cbf8d278b171ae10b696478077caa57
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-10-10 12:10:03 -0600

    fixed bad syntax

--- server/models.py
+++ server/models.py
@@ -5,6 +5,12 @@ from config import db
 
 # Models go here!
 
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+from config import db
+
+# Models go here!
+
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
@@ -24,19 +30,22 @@ class Comment(db.Model, SerializerMixin):
     __tablename__ = "comments"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(users.id), nullablle=False)
-    post_id = db.Column(db.Integer, db.ForeignKey(posts.id), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
-
 class Post(db.Model, SerializerMixin):
+    __tablename__ = "posts"
+
     id = db.Column(db.Integer, primary_key=True)
-    textbook_id = db.Column(db.Integer, db.ForeignKey(textbooks.id), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey(users.id), nullable=False)
+    textbook_id = db.Column(db.Integer, db.ForeignKey('textbooks.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     price = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
 class Watchlist(db.Model, SerializerMixin):
+    __tablename__ = "watchlists"
+
     id = db.Column(db.Integer, primary_key=True)
-    post_id = db.Column(db.Integer, db.ForeignKey(posts.id))
-    textbook_id = db.Column(db.Integer, db.ForeignKey(textbooks.id))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    textbook_id = db.Column(db.Integer, db.ForeignKey('textbooks.id'))
