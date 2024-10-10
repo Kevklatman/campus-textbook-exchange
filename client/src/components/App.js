8e67f8fab28733ad8981a117bf4284067cf4236b
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-10-10 11:56:18 -0600

    init

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    hi
+      </header>
+    </div>
+  );
+}
+
+export default App;
